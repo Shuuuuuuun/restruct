7b9b3b330e9c250cc9642fd7ccbfc8d62b834af8
kimura <s.kimura@branu.jp>
2014-07-07 17:58:22 +0900

    complemented ClassLoader

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -8,5 +8,27 @@
 
 class ClassLoader
 {
+    protected $dirs;
 
+    public function regsiter()
+    {
+        sql_autoload_register(array($this, 'loadClass'));
+    }
+
+    public function regsiterDir($dir)
+    {
+        $this->dirs[]= $dir;
+    }
+
+    public function loadClass($class)
+    {
+        foreach($this->dris as $dir){
+            $file = $dir . '/' . $class . '.php';
+            if(is_readable($file)){
+                require $file;
+
+                return;
+            }
+        }
+    }
 } 
